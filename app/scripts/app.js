a435c2a56d7415f5006fddd98c5fe8b1d9d773df
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-10-29 22:43:55 -0600

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name innovoporBootstrapApp
+ * @description
+ * # innovoporBootstrapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('innovoporBootstrapApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name innovoporBootstrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the innovoporBootstrapApp
+ */
+angular.module('innovoporBootstrapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
