48e44a28d57289669546ba8b4d897060798cf783
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-11-04 19:44:41 -0600

    new content added

--- app/scripts/controllers/innovo-por.js
+++ app/scripts/controllers/innovo-por.js
@@ -9,9 +9,8 @@
  */
 angular.module('innovoporBootstrapApp')
   .controller('InnovoPorCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  	//WordCloud(document.getElementById('cloud'), { list: [['test', 2], ['perro', 22]] } );
+
+
   });
