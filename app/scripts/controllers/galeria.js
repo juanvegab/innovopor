aa0b88413751bd1d919bd9e748af4792fb8cf765
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2014-11-18 09:14:48 -0600

    final design

--- app/scripts/controllers/galeria.js
+++ app/scripts/controllers/galeria.js
@@ -10,23 +10,40 @@
 angular.module('innovopor')
   .controller('GaleriaCtrl', ['$scope', '$http', function ($scope, $http) {
 	
-	var $ = window.$;
-  	
-  	$scope.url = 'https://graph.facebook.com/713241582067248/photos';
-  	$scope.pictures = [];
+  var $ = window.$;
+  $scope.totalItems = 1;
+  $scope.currentPage = 4;
+  $scope.maxSize = 9;
+  $scope.bigTotalItems = 175;
+  $scope.bigCurrentPage = 1;
+	
+	$scope.url = 'https://graph.facebook.com/713241582067248/photos';
+	$scope.pictures = [];
 
-  	$http.get($scope.url).then(function(response) {
-  		$scope.pictures = response.data.data;
-        $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-            event.preventDefault();
-            $(this).ekkoLightbox();
-        }); 
-  	});
+	$http.get($scope.url).then(function(response) {
+    $scope.pictures = response.data.data;
+    $scope.totalItems = $scope.pictures.length;
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
+    }); 
+	});
 
-  	$scope.openModal = function(image) {
+  $scope.openModal = function(image) {
 		var theModal = '#galleryModal';
 		var imageSRC = image.source;
 		$(theModal+' img').attr('src', imageSRC);
 		$(theModal).modal('show');
 	};
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    // $log.log('Page changed to: ' + $scope.currentPage);
+  };
+
+
+
 }]);
--- app/scripts/controllers/musica-innovadora.js
+++ app/scripts/controllers/musica-innovadora.js
@@ -11,6 +11,8 @@ angular.module('innovopor')
   .controller('MusicaInnovadoraCtrl', function ($scope) {
 
 	$scope.videoLinks = [{link: 'H6Rbj4LbapQ', caption: 'Asi se vivio la pachanga. Video 1'},{link: 'H6Rbj4LbapQ', caption: 'Asi se vivio la pachanga. Video 1'},{link: 'H6Rbj4LbapQ', caption: 'Asi se vivio la pachanga. Video 1'},{link: 'H6Rbj4LbapQ', caption: 'Asi se vivio la pachanga. Video 1'}];
+	$scope.isPlaying = false;
+	$scope.btnImage = 'images/download-ringtone.png';
 
 	var $ = window.$;
 
@@ -20,4 +22,16 @@ angular.module('innovopor')
 		$(theModal+' iframe').attr('src', videoSRC);
 		$(theModal).modal('show');
 	};
+
+	$scope.tooglePlay = function() {
+		if($scope.isPlaying){
+			$('#audio-player')[0].pause();
+			$scope.btnImage = 'images/download-ringtone.png';
+		} else {
+			$('#audio-player')[0].play();
+			$scope.btnImage = 'images/download-ringtone-pausa.png';
+		}
+		$scope.isPlaying = !$scope.isPlaying;
+	};
+
   });
